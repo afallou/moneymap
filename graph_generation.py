6c3a468a986aa26da5d7cc3f84b85ee95a9ecde9
Adrien Fallou <adrien.fallou@gmail.com>
2014-10-30 14:21:57 -0700

    Make contributions.json well-formatted, first code to generate the graph of politicians

--- graph_generation.py
+++ graph_generation.py
@@ -1,12 +1,35 @@
 import snap
 import json
+import simplejson
+
+REPUBLICAN = ''
+DEMOCRAT = ''
+INDEPENDENT = ''
+THIRD = ''
 
 def loadData():
     data = {}
-    with open('json_file.json') as jsonData:
-        data = json.load(jsonData)
+    with open('contributions.json') as contributions:
+        data = json.load(contributions)
     return data
 
-def generateGraph:
+def generateGraph():
+    contributions = loadData()
     graph = snap.PNGraph.New()
+    sources = {}
+    for contribution in contributions:
+        pid = contribution["recipient_id"]
+        for contributor in contribution["contributors"]:
+            if contributor in sources.keys():
+                if not pid in sources[contributor]:
+                    sources[contributor].append(pid)
+            else:
+                sources[contributor] = [pid]
+    links = []
+    for contributor, recipients in sources.iteritems():
+        for i in xrange(len(recipients)):
+            for j in xrange(i + 1, len(recipients)):
+                links.append( {"source": recipients[i], "target": recipients[j]} )
+    with open('recipients_graph.json', 'w') as rf:
+        rf.write(simplejson.dumps(links, indent=4))
     
