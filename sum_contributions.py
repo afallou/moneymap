aef3a0078be8a66a9edbaa54999ffb85da4952f5
Adrien Fallou <adrien.fallou@gmail.com>
2014-11-05 22:49:15 -0800

    Add breakdowns in sums

--- sum_contributions.py
+++ sum_contributions.py
@@ -1,10 +1,20 @@
 import json
+from collections import Counter
 
 if __name__ == "__main__":
-    sum = 0
+    total_sum = 0
+    reasons = Counter()
+    reason_sums = Counter()
+
     with open('contributions.json') as c:
         contributions = json.load(c)
         for recipient in contributions:
             for contribution in recipient["contributors"]:
-                sum += float(contribution["amount"])
-    print '%e' % sum
+                total_sum += float(contribution["amount"])
+                reasons[contribution["transaction_type_description"]] += 1
+                reason_sums[contribution["transaction_type_description"]] += float(contribution["amount"])
+
+    print 'Total sum: %e' % total_sum
+    print "Total count:", sum(reasons.values())
+    print "Breakdown count", reasons
+    print "Breakdown sums", reason_sums
