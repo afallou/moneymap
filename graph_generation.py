619d29493f2c8c5c0ee75e6275f2142656753c57
Adrien Fallou <adrien.fallou@gmail.com>
2014-11-08 14:41:31 -0800

    Much, much faster graph generation

--- graph_generation.py
+++ graph_generation.py
@@ -7,15 +7,13 @@ DEMOCRAT = ''
 INDEPENDENT = ''
 THIRD = ''
 
-def loadData():
+def load_data():
     data = {}
-    with open('contributions_small.json') as contributions:
+    with open('contributions.json') as contributions:
         data = json.load(contributions)
     return data
 
-def generateGraph():
-    contributions = loadData()
-    graph = snap.PNGraph.New()
+def create_recipients_list(contributions):
     sources = {}
     nodes = []
     for contribution in contributions:
@@ -23,18 +21,27 @@ def generateGraph():
         nodes.append({"id": pid})
         for contributor in contribution["contributors"]:
             cid = contributor["contributor_ext_id"]
-            if cid in sources.keys():
+            if cid in sources:
                 if not pid in sources[cid]:
                     sources[cid].append(pid)
             else:
                 sources[cid] = [pid]
-    links = []
+    return sources, nodes
+
+def create_edges(sources):
+    edges = []
     for contributor, recipients in sources.iteritems():
         for i in xrange(len(recipients)):
             for j in xrange(i + 1, len(recipients)):
-                links.append( {"source": recipients[i], "target": recipients[j]} )
+                edges.append( {"source": recipients[i], "target": recipients[j]} )
+    return edges                
+
+def generate_graph():
+    contributions = load_data()
+    sources, nodes = create_recipients_list(contributions)
+    edges = create_edges(sources)
     with open('recipients_graph.json', 'w') as rf:
-        rf.write(simplejson.dumps({"nodes": nodes, "links": links}, indent=4))
+        rf.write(simplejson.dumps({"nodes": nodes, "links": edges}, indent=4))
         
 if __name__ == "__main__":
-    generateGraph()
+    generate_graph()
